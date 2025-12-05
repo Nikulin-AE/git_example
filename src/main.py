bcf80662477a50c0e56d280dc9a407d413236718
nikulin <anton.nikulin@atom.team>
2025-12-05 18:24:27 +0300

    added annotations

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-def greet(name):
+def greet(name: str):
     """Выводит приветствие на консоль.
 
     Args:
@@ -7,5 +7,5 @@ def greet(name):
     print(f"Привет, {name}!")
 
 if __name__ == "__main__":
-    user_name = input("Как тебя зовут? ")
+    user_name: str = input("Как тебя зовут? ")
     greet(user_name)
