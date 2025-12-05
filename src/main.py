54d5078bf0c87c9fc3d03029910d4687dc4ffbc3
nikulin <anton.nikulin@atom.team>
2025-12-05 18:24:00 +0300

    added surname

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
-def greet(name):
+def greet(name, surname):
     """Выводит приветствие на консоль.
 
     Args:
         name (str): Имя пользователя.
     """
-    print(f"Привет, {name}!")
+    print(f"Привет, {name} {surname}!")
 
 if __name__ == "__main__":
     user_name = input("Как тебя зовут? ")
-    greet(user_name)
+    surname = input("Какая у вас фамилия? ")
+    greet(user_name, surname)
